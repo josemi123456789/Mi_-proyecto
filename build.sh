69788d084d8dc22973c91dd33e64b2706629a908
josemi123456789 <messicevallos21@gmail.com>
2026-02-12 23:54:02 -0500

    Enhance build script with error handling and user creation
    Add error handling and create superuser if not exists

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input || true
+
+# Crea el usuario 'admin' con la clave 'Josemi2026*' si no existe
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'Josemi2026*')"
