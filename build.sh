fd9f1b13186d123e48d720ca20600794f96da0ac
josemi123456789 <messicevallos21@gmail.com>
2026-02-13 00:08:57 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,10 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Este comando crea el usuario y nos avisa si funcionó
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='admin').exists(): \
-    User.objects.create_superuser('admin', 'admin@example.com', 'Josemi2026*'); \
-    print('>>> SUPERUSUARIO CREADO: admin / Josemi2026*'); \
-else: \
-    print('>>> EL SUPERUSUARIO YA EXISTE')"
+# Crea el admin solo si no existe (Versión de una sola línea segura)
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'Josemi2026*')"
